474f4a4ba8d13cee02b8cc40a0244fd4db4c536e
Rohit Sharmilee <rohitsharmilee@Rohits-MacBook-Air.local>
2024-09-08 17:47:58 +0530

    Fourth Summit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import restart from 'vite-plugin-restart'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/your-subdirectory/',  // Adjust this to the correct path
+    base: "",  // Adjust this to the correct path
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
